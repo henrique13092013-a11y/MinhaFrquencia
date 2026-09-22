d1652095b071f2c9cb2829e7e108f7d575c4bbb0
henrique13092013-a11y <henrique13092013@gmail.com>
2026-09-22 19:17:25 -0300

    Atualiza service worker para v1.0

--- sw.js
+++ sw.js
@@ -1,13 +1,24 @@
-const CACHE='minhafrequencia-pages-v9-graphic';
+const CACHE='minhafrequencia-v1.0.0';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   const req=event.request;
   if(req.mode==='navigate'){
-    event.respondWith(fetch(req).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));return resp}).catch(()=>caches.match('./index.html')));
+    event.respondWith(fetch(req).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>c.put('./index.html',copy));
+      return resp;
+    }).catch(()=>caches.match('./index.html')));
     return;
   }
-  event.respondWith(fetch(req).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(req,cp));return resp}).catch(()=>caches.match(req)));
+  event.respondWith(caches.match(req).then(hit=>{
+    const network=fetch(req).then(resp=>{
+      const copy=resp.clone();
+      caches.open(CACHE).then(c=>c.put(req,copy));
+      return resp;
+    }).catch(()=>hit);
+    return hit||network;
+  }));
 });
