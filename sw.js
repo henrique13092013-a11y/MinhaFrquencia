c28050e72419ba2396631dbadabee9abe180afee
henrique13092013-a11y <henrique13092013@gmail.com>
2026-09-22 19:49:30 -0300

    Atualiza cache PWA para v1.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='minhafrequencia-v1.1.0';
+const CACHE='minhafrequencia-v1.2.0';
 const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
@@ -6,19 +6,11 @@ self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET')return;
   const req=event.request;
   if(req.mode==='navigate'){
-    event.respondWith(fetch(req).then(resp=>{
-      const copy=resp.clone();
-      caches.open(CACHE).then(c=>c.put('./index.html',copy));
-      return resp;
-    }).catch(()=>caches.match('./index.html')));
+    event.respondWith(fetch(req).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put('./index.html',copy));return resp}).catch(()=>caches.match('./index.html')));
     return;
   }
   event.respondWith(caches.match(req).then(hit=>{
-    const network=fetch(req).then(resp=>{
-      const copy=resp.clone();
-      caches.open(CACHE).then(c=>c.put(req,copy));
-      return resp;
-    }).catch(()=>hit);
+    const network=fetch(req).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(req,copy));return resp}).catch(()=>hit);
     return hit||network;
   }));
 });
