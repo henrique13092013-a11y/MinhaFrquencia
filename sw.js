fa922ace6472436f8ce1b06657bda7e4eb5d0dc5
henrique13092013-a11y <henrique13092013@gmail.com>
2026-09-22 00:34:07 -0300

    Atualiza cache da PWA para novas versões

--- sw.js
+++ sw.js
@@ -1,16 +1,41 @@
-const CACHE = 'minhafrequencia-pages-v1';
-const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon.svg'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+const CACHE='minhafrequencia-pages-v3';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting()));
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys()
+      .then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
+      .then(()=>self.clients.claim())
+  );
 });
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  event.respondWith(caches.match(event.request).then(hit => hit || fetch(event.request).then(resp => {
-    const copy = resp.clone();
-    caches.open(CACHE).then(cache => cache.put(event.request, copy));
-    return resp;
-  }).catch(() => caches.match('./index.html'))));
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  const req=event.request;
+  if(req.mode==='navigate'){
+    event.respondWith(
+      fetch(req)
+        .then(resp=>{
+          const copy=resp.clone();
+          caches.open(CACHE).then(cache=>cache.put('./index.html',copy));
+          return resp;
+        })
+        .catch(()=>caches.match('./index.html'))
+    );
+    return;
+  }
+  event.respondWith(
+    caches.match(req).then(hit=>{
+      const network=fetch(req).then(resp=>{
+        const copy=resp.clone();
+        caches.open(CACHE).then(cache=>cache.put(req,copy));
+        return resp;
+      }).catch(()=>hit);
+      return hit||network;
+    })
+  );
 });
